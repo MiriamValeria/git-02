f40a172e58862de443c5164202b44474e2837e2d
Miriam Valeria <20253ds032@utez.edu.mx>
2026-05-20 13:50:06 -0600

    [NEW] clase calculadora y constructor

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Calculadora {
+
+    private double numero1;
+    private double numero2;
+
+    public Calculadora(double numero, double numero2) {
+        this.numero1 = numero;
+        this.numero2 = numero2;
+    }
+
+
+
+
+
+
+
+
+
+}
